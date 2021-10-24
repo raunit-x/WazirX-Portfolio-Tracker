bfad06f8253922d53cc2518e86c571b2668d4352
raunit.dalal <raunit.dalal@sprinklr.com>
2021-10-24 12:42:25 +0530

    continuous data fetch

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pandas as pd
 from token_constants import *
 from terminal_formatting import *
 from sty import fg, bg, ef, rs
+import sys
 
 
 def get_payloads(ticker: str, payloads: dict):
@@ -36,36 +37,35 @@ def get_value_per_token(payloads: dict, trading_report: TradingReport) -> dict:
 
 
 def generate_holdings_report(token_info: dict, trading_report: TradingReport) -> pd.DataFrame:
-    report_df = pd.DataFrame(columns=['TOKEN', 'CURRENT PRICE', 'HOLDINGS', 'INITIAL INVESTMENT',
+    report_df = pd.DataFrame(columns=['TOKEN', 'CURRENT PRICE', 'HOLDINGS', 'INVESTMENT',
                                       'CURRENT VALUE', 'GAINS (%)'])
     for i, tok in enumerate(trading_report.holdings):
         gains = 100 * ((token_info[tok][TOTAL_VALUE] / trading_report.investment_per_token[tok]) - 1)
         report_df.loc[i] = [tok, token_info[tok][BUY], Decimal(trading_report.holdings[tok]), trading_report.investment_per_token[tok],
                             token_info[tok][TOTAL_VALUE], gains]
-    report_df.sort_values(by='INITIAL INVESTMENT', ascending=False, inplace=True, ignore_index=True)
+    report_df.sort_values(by='CURRENT VALUE', ascending=False, inplace=True, ignore_index=True)
     return report_df
 
 
 def print_report(report_df: pd.DataFrame, trading_report: TradingReport):
-    print()
     total_investment = trading_report.total_deposits
     total_current_value = report_df['CURRENT VALUE'].sum()
     gains = (total_current_value / total_investment - 1) * 100
     gains_total = total_current_value - total_investment
-    print(f"\n{ef.bold}{BColors.FAIL}INITIAL INVESTMENT: {RUPEE}{total_investment:.2f}")
-    color = BColors.OKGREEN if total_current_value > total_investment else BColors.FAIL
-    print(f"{ef.bold}{color}CURRENT PORTFOLIO : {RUPEE}{total_current_value:.2f}")
-    print(f"{ef.bold}{color}GAINS : {RUPEE}{gains_total:.2f} ({UP_ARROW if gains > 0 else DOWN_ARROW} {gains:.2f} %)")
-    print()
-    color_encoding = [fg.li_cyan, (fg.li_red, fg.li_green), fg.cyan, fg.grey, (fg.li_red, fg.li_green),
-                      (fg.li_red, fg.li_green)]
+    print(f"\n{ef.bold}{fg.yellow}INITIAL INVESTMENT: {RUPEE}{total_investment:.2f}\n", end='\r')
+    color = fg.da_green if total_current_value > total_investment else fg.da_red
+    print(f"{ef.bold}{color}CURRENT PORTFOLIO : {RUPEE}{total_current_value:.2f}\n", end='\r')
+    print(f"{ef.bold}{color}GAINS : {RUPEE}{gains_total:.2f} ({UP_ARROW if gains > 0 else DOWN_ARROW} {gains:.2f} %)\n\n\n",
+          end='\r')
+    # print()
+    color_encoding = [fg.li_cyan, (fg.li_red, fg.da_green), fg.cyan, fg.yellow, (fg.li_red, fg.da_green),
+                      (fg.li_red, fg.da_green)]
     text_formatting = [ef.bold, ef.bold, ef.bold, '', ef.bold, ef.bold]
     prefixes = ['', RUPEE, '', RUPEE, RUPEE, (UP_ARROW, DOWN_ARROW)]
-    for col in report_df.columns:
-        print(f"{ef.bold}{fg.da_magenta}{col:<22}{BColors.ENDC}", end='')
-    print()
-    print()
+    column_string = ''.join([f"{ef.bold}{fg.da_magenta}{col:18}{BColors.ENDC}" for col in report_df.columns])
+    print(f"{column_string}\n\n", end='\x1b[1K\r')
     for i in range(len(report_df)):
+        row_string = ''
         for j in range(len(report_df.columns)):
             color = color_encoding[j]
             text_fmt = text_formatting[j]
@@ -85,32 +85,36 @@ def print_report(report_df: pd.DataFrame, trading_report: TradingReport):
                 val = f"{prefix} {val} %"
             else:
                 val = f"{prefix}{val}"
-            print(f"{text_fmt}{color}{val:<22}", end='')
-        print()
-        print()
+            row_string += f"{text_fmt}{color}{val:18}"
+        print(f"{row_string}\n\n", end='\r')
 
 
 def main():
-    for i in range(5):
-        time.sleep(0.02)
-        progress_bar(i, 10)
-    trading_report_path = '/Users/raunitdalal/PycharmProjects/myCryptoApp/Trading Reports/trading_report.xlsx'
-    payloads = {}
-    get_payloads(USDT, payloads)
-    trading_report = TradingReport(trading_report_path, Decimal(payloads[USDT][TICKER][BUY]))
-    getcontext().prec = 10
-    pool_size = 16
-    pool = Pool(pool_size)
-    for ticker in trading_report.holdings:
-        pool.apply_async(get_payloads, (ticker, payloads))
-    for i in range(5, 10):
-        time.sleep(0.01)
-        progress_bar(i, 10)
-    pool.close()
-    pool.join()
-    token_info = get_value_per_token(payloads, trading_report)
-    report_df = generate_holdings_report(token_info, trading_report)
-    print_report(report_df, trading_report)
+    while True:
+        trading_report_path = '/Users/raunitdalal/PycharmProjects/myCryptoApp/Trading Reports/trading_report.xlsx'
+        payloads = {}
+        get_payloads(USDT, payloads)
+        trading_report = TradingReport(trading_report_path, Decimal(payloads[USDT][TICKER][BUY]))
+        getcontext().prec = 10
+        pool_size = 16
+        pool = Pool(pool_size)
+        for ticker in trading_report.holdings:
+            pool.apply_async(get_payloads, (ticker, payloads))
+        pool.close()
+        pool.join()
+        # print(f"\n Hello ", end='\r')
+        token_info = get_value_per_token(payloads, trading_report)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        # for i in range(10):
+        #     time.sleep(0.02)
+        #     progress_bar(i, 10)
+        report_df = generate_holdings_report(token_info, trading_report)
+        print_report(report_df, trading_report)
+        for i in range(10, -1, -1):
+            time.sleep(1)
+            i = f'{i:0}'
+            print(f" {fg.da_green}REFRESHING THE DATA IN: {i.zfill(2)} SECONDS", end='\r')
+        # time.sleep(10)
 
 
 if __name__ == '__main__':
